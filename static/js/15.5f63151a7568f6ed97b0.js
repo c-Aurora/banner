bffea102e1092c7f9b9a078daccc1b1e32b0a42b
c-Aurora <171968025@qq.com>
2023-04-13 22:16:02 +0800

    Delete static directory

--- static/js/15.5f63151a7568f6ed97b0.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([15],{"L+gl":function(l,e,u){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var n=u("VU/8")(null,null,!1,null,null,null);e.default=n.exports}});
-//# sourceMappingURL=15.5f63151a7568f6ed97b0.js.map
