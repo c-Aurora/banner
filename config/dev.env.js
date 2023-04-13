a3273a2706d2bf56c991a0ea12428cd41f4ad0b2
c-Aurora <171968025@qq.com>
2023-04-13 14:13:35 +0800

    Delete config directory

--- config/dev.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-    NODE_ENV: '"development"'
-    // BASE_API: '"https://mock.mengxuegu.com/mock/642f916f32affa39a121ce38/routing"',
-})
--- config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-    NODE_ENV: '"production"'
-    // BASE_API: '"https://mock.mengxuegu.com/mock/642f916f32affa39a121ce38/routing"',
-    
-}
